5a314086e0b988ed6320b52c18c52ffe9b0010c0
deni-rocha <denilsonrocha_@outlook.com>
2025-05-28 19:13:11 -0300

    muda o serviço de email

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -1,31 +1,46 @@
-import sgMail from '@sendgrid/mail';
 import dotenv from 'dotenv';
+import nodemailer from 'nodemailer';
 
 dotenv.config({ path: '.env' });
-sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
+
+
+const { EMAIL, EMAIL_PASS } = process.env;
+
+
+
+// Crie o transporter com as configurações do seu servidor SMTP
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com', // ou outro servidor SMTP
+  port: 587,
+  secure: false, // true para 465, false para outras portas
+  auth: {
+    user: EMAIL,
+    pass: EMAIL_PASS,
+  }
+});
 
 export const sendVerificationEmail = async (to: string, token: string) => {
     const verificationLink = `${process.env.BASE_URL}/user/verify?token=${token}`;
 
-    const msg = {
-        to,
-        from: 'frevolink@gmail.com', // Deve ser verificado no SendGrid
-        subject: 'Confirmação de E-mail - FLpay',
-        html: `
-      <div style="font-family: Arial, sans-serif; line-height: 1.5; color: #333;">
-        <h2 style="color: #4CAF50;">Bem-vindo ao FLpay!</h2>
-        <p>Obrigado por se registrar. Para começar, por favor confirme seu e-mail clicando no link abaixo:</p>
-        <p style="text-align: center; margin: 20px 0;">
-          <a href="${verificationLink}" style="background-color: #4CAF50; color: white; padding: 10px 20px; text-decoration: none; border-radius: 5px;">Verificar E-mail</a>
-        </p>
-        <p>Se você não se registrou no FLpay, ignore este e-mail.</p>
-        <p>Atenciosamente,<br/>Equipe FLpay</p>
-      </div>
-    `,
-    };
-
+    
     try {
-        await sgMail.send(msg);
+      await transporter.sendMail({
+      from: '"Ralce Store" frevolink@email.com',
+      to,
+      subject: 'Confirmação de E-mail - FLpay',
+      html: `
+        <div style="font-family: Arial, sans-serif; line-height: 1.5; color: #333;">
+          <h2 style="color: #4CAF50;">Bem-vindo ao FLpay!</h2>
+          <p>Obrigado por se registrar. Para começar, por favor confirme seu e-mail clicando no link abaixo:</p>
+          <p style="text-align: center; margin: 20px 0;">
+            <a href="${verificationLink}" style="background-color: #4CAF50; color: white; padding: 10px 20px; text-decoration: none; border-radius: 5px;">Verificar E-mail</a>
+          </p>
+          <p>Se você não se registrou no FLpay, ignore este e-mail.</p>
+          <p>Atenciosamente,<br/>Equipe FLpay</p>
+        </div>
+      `
+    });
+      
         console.log('E-mail enviado via SendGrid!');
     } catch (error) {
         console.error('Erro ao enviar e-mail:', error);
